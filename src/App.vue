394f37d8cdbed251ebb6fb6d1b40b61a67e684a9
phuongNam <lephuongnam11062001@gmail.com>
2022-10-01 10:49:19 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+  export default {
+
+  }
+</script>
+
+<template>
+  <h1>Hello, vue.js!</h1>
+</template>
+
+<style>
+  .page {
+    max-width: 400px;
+    margin: auto;
+    
+  }
+  h1 {
+    color: red;
+    background-color: aqua;
+    border-radius: 10px;
+    border: 1px solid red;
+  }
+</style>
